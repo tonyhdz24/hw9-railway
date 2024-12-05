cbe7d6fa8148b6cc3cd9a291d5d1deb5707daa0e
Tony Hernandez <tonyhernandez24@u.boisestate.edu>
2024-12-04 20:10:36 -0700

    debugging

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -59,7 +59,7 @@ public class HerokuApplication {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate(
           "CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
-      stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
+      stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + "TESTSS" + "')");
       ResultSet rs = stmt.executeQuery("SELECT tick, random_string FROM table_timestamp_and_random_string");
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
